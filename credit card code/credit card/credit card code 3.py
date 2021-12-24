cbd45aa2f4cf2c3b3391fc7c009e103691422f66
afsar.ansari <afsaralam7200@gmail.com>
2021-12-24 13:29:53 +0530

    credit card fraud

---
+++ credit card code/credit card/credit card code 3.py
@@ -0,0 +1,79 @@
+#importing the modules
+#import numpy n-dimensional array
+import numpy as np
+#import sklearn python machine learning  modules
+import sklearn as sk
+#import pandas dataframes
+import pandas as pd
+#import matplotlib for plotting
+import matplotlib.pyplot as plt
+#import datasets  and linear_model from sklearn module
+from sklearn import datasets, linear_model
+#import Polynomial features from sklearn module
+from sklearn.preprocessing import PolynomialFeatures
+#import train_test_split data classification
+from sklearn.model_selection import train_test_split
+#import ConfusionMatrix from pandas_ml
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.metrics import confusion_matrix
+#reading the csv file from C:/Python27
+dataframe = pd.read_csv('creditcard.csv', low_memory=False)
+#dataframe.sample Returns a random sample of items from an axis of object.
+#The frac keyword argument specifies the fraction of rows to return in the random sample, so frac=1 means return all rows (in random order).
+# If you wish to shuffle your dataframe in-place and reset the index
+dataframe = dataframe.sample(frac=1).reset_index(drop=True)
+#dataframe.head(n) returns a DataFrame holding the first n rows of dataframe.
+print(dataframe.head(10))
+
+#The loc method gives direct access to the dataframe allowing for assignment to specific locations of the dataframe.
+#here in dataframe class with 1 label is selected for fraud_class
+fraud_class = dataframe.loc[dataframe['Class'] == 1]
+#here in dataframe class with 1 label is selected for non_fraud_class
+non_fraud_class = dataframe.loc[dataframe['Class'] == 0]
+#printing length of fraud_class and non_fraud class
+print("Totally we have ", len(fraud_class), "fraud data class point  and", len(non_fraud_class), "nonfraudulent data class points.")
+#plotting fraudplot for fraud_class
+ax = fraud_class.plot.scatter(x='Amount', y='Class', color='Red', label='Fraud')
+#plotting plot for non_fraud_class with fraud_class
+non_fraud_class.plot.scatter(x='Amount', y='Class', color='Green', label='Normal', ax=ax)
+plt.show()
+print("This Feature what is mentioned is based on the class Distribution.")
+#Let us see the plot zooming only  fraudplot
+bx = fraud_class.plot.scatter(x='Amount', y='Class', color='Orange', label='Fraud')
+#Showing the plot
+plt.show()
+#Again plotting non_fraud_class plot with fraud_class plot
+ax = fraud_class.plot.scatter(x='V15', y='Class', color='Orange', label='Fraud')
+non_fraud_class.plot.scatter(x='V15', y='Class', color='Blue', label='Normal', ax=ax)
+plt.show()
+#Dataframes for  X all the columns except class and y class columns
+X = dataframe.iloc[:,:-1]
+y = dataframe['Class']
+#Finding the length of X and y
+print("X and y sizes, respectively:", len(X), len(y))
+#Splitting the training and Testing data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35, random_state=500)
+#Calculating the data
+print("Train and test sizes, respectively:", len(X_train), len(y_train), "|", len(X_test), len(y_test))
+print("Total number of frauds:", len(y.loc[dataframe['Class'] == 1]), len(y.loc[dataframe['Class'] == 1])/len(y))
+print("Number of frauds on y_test:", len(y_test.loc[dataframe['Class'] == 1]), len(y_test.loc[dataframe['Class'] == 1]) / len(y_test))
+print("Number of frauds on y_train:", len(y_train.loc[dataframe['Class'] == 1]), len(y_train.loc[dataframe['Class'] == 1])/len(y_train))
+#Applying Logistic Regression Machine Learning Algorithm
+logistic = linear_model.LogisticRegression(C=1e5)
+#Fitting the Algorithm for X_train and y_train
+logistic.fit(X_train, y_train)
+#Scoring
+print("Score: ", logistic.score(X_test, y_test))
+y_predicted = np.array(logistic.predict(X_test))
+y_right = np.array(y_test)
+#print y_test
+#The confusion matrix (or error matrix) is one way to summarize the performance of a classifier
+#  for binary classification tasks. This square matrix
+# consists of columns and rows that list the number of instances as absolute or
+# relative "actual class" vs. "predicted class" ratios.
+#Plotting the Confusion matrix for y_right and y_predicted
+confusion_matrix = confusion_matrix(y_right, y_predicted)
+print("Confusion matrix:",confusion_matrix)
+
+
